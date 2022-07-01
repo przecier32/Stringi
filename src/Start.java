196628f796778b175fc5baedaa7d449efecee863
Piotr <wolkanin.piotr@icloud.com>
2022-07-01 13:39:58 +0200

    some code lines

---
+++ src/Start.java
@@ -0,0 +1,31 @@
+
+
+public class Start {
+    public static void main(String[] args) {
+
+        String one = "One";
+        String three = new String("Two");
+
+
+        System.out.println(one);
+        int length;
+        length = one.length();
+        System.out.println(length);
+
+        one = one.replace('e','c');
+
+        one = one.concat("e")
+                 .concat("Upon")
+                 .concat("aTime");
+
+        System.out.println(one);
+
+
+        boolean a = three.contains("o");
+        System.out.println(a);
+
+        
+
+
+    }
+}
